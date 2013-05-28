f74ee6f3543f1e8fc67ce9d4abb346e3ed169559
Harsh <basis.spline@gmail.com>
2013-05-28 12:15:20 +0530

    Python Script added

---
+++ ip.py
@@ -0,0 +1,17 @@
+def fqdn():
+    import socket
+    fi=open('fqdn.txt','r')
+    fo=open('ip.csv','w')
+    fo.write('IP Address'+','+'Host Name'+'\n')
+    for i in fi:
+        #print i
+        try:
+            a=socket.gethostbyname(i.strip())
+        except:
+            a=['Not Found',',',i.strip()]
+        print a
+        fo.write(i.strip()+ ',' +a+'\n')
+    fi.close()
+    fo.close()
+
+fqdn()
